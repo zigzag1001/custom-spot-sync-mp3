5a2a72e8eda50a86b113c8e94482da5d6701f542
mele <mele@mele.mele.local>
2024-11-20 20:28:14 -0500

    .

--- main.py
+++ main.py
@@ -7,26 +7,43 @@ import get_dupes
 import dotenv
 
 OUTPUT_PATH = "./music/"
+def main():
+    with open(".env", "r") as f:
+        if "SPOTIFY_REFRESH_TOKEN" not in f.read():
+            get_spot_access_token.get_first_spot_access_token()
 
-with open(".env", "r") as f:
-    if "SPOTIFY_REFRESH_TOKEN" not in f.read():
-        get_spot_access_token.get_first_spot_access_token()
 
+    config = dotenv.dotenv_values()
 
-config = dotenv.dotenv_values()
+    access_token = get_spot_access_token.refresh_spot_access_token(config["SPOTIFY_REFRESH_TOKEN"])['access_token']
 
-access_token = get_spot_access_token.refresh_spot_access_token(config["SPOTIFY_REFRESH_TOKEN"])['access_token']
+    headers = {
+            "Authorization": f"Bearer {access_token}",
+    }
 
-headers = {
-        "Authorization": f"Bearer {access_token}",
-}
+    playlists = get_user_playlists.get_user_playlists(headers)
 
-playlists = get_user_playlists.get_user_playlists(headers)
+    new_files = download_spot_playlists.download_spot_playlists(playlists)
 
-new_files = download_spot_playlists.download_spot_playlists(playlists)
+    retag_spotdl_dls.retag_spotdl_dls(new_files)
 
-retag_spotdl_dls.retag_spotdl_dls(new_files)
+    hq_320_m3u8.main()
 
-hq_320_m3u8.main()
+    get_dupes.get_dupes()
 
-get_dupes.get_dupes()
+try:
+    main()
+except:
+    import requests
+    config = dotenv.dotenv_values()
+
+    ntfy_url = config["NTFY_url"]
+
+    headers = {
+        "t": "Spotify Synced"
+    }
+    data = "EXCEPTION IN PYTHON"
+
+    response = requests.post(ntfy_url, headers=headers, data=data)
+
+    print(response.status_code)
--- run_for_cron.sh
+++ run_for_cron.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/mele/mele_config/containers/custom-spot-sync-mp3
 sudo docker compose up --no-recreate
-curl -H "t: Spotify Synced" -d "Navidrome Synced" ntfy.sh/fail2ban_zmc_weekoldroadkill
