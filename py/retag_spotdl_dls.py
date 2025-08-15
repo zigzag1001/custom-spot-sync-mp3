99d2bcddbed81bf590e7ad6d3f01312fa6129fa8
mele <mele@mele.local>
2025-08-15 01:20:29 -0400

    fix opus retagging

--- py/retag_spotdl_dls.py
+++ py/retag_spotdl_dls.py
@@ -1,6 +1,6 @@
 import os
-import dotenv
 import eyed3
+from mutagen.oggopus import OggOpus
 
 eyed3.log.setLevel("ERROR")
 
@@ -8,24 +8,31 @@ OUTPUT_PATH = "./music/"
 
 # retag song to only have one artist, keep all other metadata
 
-config = dotenv.dotenv_values()
-
 def retag_spotdl_dls(new_files):
     retagged = []
     for file in new_files:
-        if not file.endswith(".mp3"):
-            continue
-        audiofile = eyed3.load(os.path.join(OUTPUT_PATH, file))
-        artists = audiofile.tag.artist
-        if '/' in artists:
-            audiofile.tag.artist = artists.split('/')[0]
-            audiofile.tag.save()
-            retagged.append(file)
-        elif '; ' in artists:
-            audiofile.tag.artist = artists.split('; ')[0]
-            audiofile.tag.save()
-            retagged.append(file)
+        path = os.path.join(OUTPUT_PATH, file)
 
-    retagged_str = '\n'.join(retagged)
+        if file.endswith(".mp3"):
+            audiofile = eyed3.load(path)
+            if not audiofile or not audiofile.tag or not audiofile.tag.artist:
+                print(f"Skipping {file}, no artist tag found.")
+                continue
+            artists = audiofile.tag.artist
+            if '/' in artists:
+                audiofile.tag.artist = artists.split('/')[0]
+                audiofile.tag.save()
+                retagged.append(file)
 
-    print(f"Retagged files:\n{retagged_str}")
+        elif file.endswith(".opus"):
+            audiofile = OggOpus(path)
+            artists = audiofile.get("artist", [None])
+            if not artists:
+                print(f"Skipping {file}, no artist tag found.")
+                continue
+            if len(artists) > 1:
+                audiofile["artist"] = artists[0]
+                audiofile.save()
+                retagged.append(file)
+
+    print("Retagged files:\n" + "\n".join(retagged))
