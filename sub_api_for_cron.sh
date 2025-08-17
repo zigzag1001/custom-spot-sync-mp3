1b2e01ee938e96c447a8516690c9495e429f644e
debian <debian@debian.proxmox.debian>
2025-08-17 11:28:41 -0400

    spotify playlist whitelist bug

--- sub_api_for_cron.sh
+++ sub_api_for_cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/mele/mele_config/containers/custom-spot-sync-mp3
+cd /home/debian/mele_config/containers/custom-spot-sync-mp3
 sudo docker compose -f compose.sub_api.yml up --no-recreate
